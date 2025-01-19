5cdb68fe5aef871f2ea1a500cd5443d2a6f0b70c
Matteo Convertino <74453395+matteo-convertino@users.noreply.github.com>
2025-01-19 19:31:29 +0100

    delete changeMessageStatus

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,8 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-// const { changeMessageStatus } = require('./changeMessageStatus');
 const { changeStatusAndLastMessage } = require('./changeStatusAndLastMessage');
 const { sendNotification } = require('./sendNotifications');
 
-// exports.changeMessageStatus = changeMessageStatus;
 exports.changeStatusAndLastMessage = changeStatusAndLastMessage;
 exports.sendNotification = sendNotification;
